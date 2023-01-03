63e51fbb2ffc2caf837eb95df76d780415fe2320
ZhangTonghai <tonghaizhang@gmail.com>
2023-01-03 22:40:46 +0800

    initial workspace and add basic project

---
+++ Basic/Basic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic
+//
+//  Created by Haizi on 2023/1/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
